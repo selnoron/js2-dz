cd665c8691cff9baa7897655f7b8e9d6b4559c5f
selnoron <124687107+selnoron@users.noreply.github.com>
2023-02-15 15:55:51 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,123 @@
+// // 1
+// let a = parseInt(prompt())
+// if (a % 2 == 0){
+//     console.log("Yes")
+// }
+// else{
+//     console.log("No")
+// }
+
+// // 2
+// let a = parseInt(prompt())
+// let b = parseInt(prompt())
+// if (a > b){
+//     console.log(b)
+// }
+// else if (a == b){
+//     console.log(a, b)
+// }
+// else{
+//     console.log(a)
+// }
+
+// // 3
+// let a = parseInt(prompt())
+// if (a > 0){
+//     console.log("+")
+// }
+// else if (a == 0){
+//     console.log("0")
+// }
+// else{
+//     console.log("-")
+// }
+
+// // 4
+// let a = parseInt(prompt())
+// let b = parseInt(prompt())
+// if (a > b){
+//     console.log(b, a)
+// }
+// else if (a == b){
+//     console.log('=')
+// }
+// else{
+//     console.log(a, b)
+// }
+
+// // 5
+// let a = parseInt(prompt())
+// let b = parseInt(prompt())
+// let c = parseInt(prompt())
+// let d = parseInt(prompt())
+// let e = parseInt(prompt())
+// if (((a + b + c + d + e) / 5) >= 4){
+//     console.log("OK")
+// }
+// else{
+//     console.log("NOT OK")
+// }
+
+// // 6
+// let a = parseInt(prompt())
+// if (a % 2 == 0){
+//     console.log(a * 3)
+// }
+// else{
+//     console.log(a / 2)
+// }
+
+// // 7
+// let a = parseInt(prompt())
+// let b = parseInt(prompt())
+// let calc = prompt()
+// if (calc == "*"){
+//     console.log(a * b)
+// }
+// if (calc == "/"){
+//     console.log(a / b)
+// }
+// if (calc == "+"){
+//     console.log(a + b)
+// }
+// if (calc == "-"){
+//     console.log(a - b)
+// }
+
+// // 8
+// let a = parseInt(prompt())
+// if (a < 0 || a > 112){
+//     console.log("Error")
+// }
+// else if (a % 10 == 1){
+//     console.log(`вам ${a} год`)
+// }
+// else if (a % 10 >= 5 && a % 10 <= 9){
+//     console.log(`вам ${a} лет`)
+// }
+// else{
+//     console.log(`вам ${a} года`)
+// }
+
+// // 9
+// let a = parseInt(prompt())
+// let b = parseInt(prompt())
+// if (a * b < 0){
+//     console.log((a * b) * -2)
+// }
+// else{
+//     console.log((a * b) * 3)
+// }
+
+// // 10
+// let a = parseInt(prompt())
+// let b = parseInt(prompt())
+// if (a > b){
+//     console.log(a - b)
+// }
+// else if (a == b){
+//     console.log(a + " = " + b)
+// }
+// else{
+//     console.log(b - a)
+// }
